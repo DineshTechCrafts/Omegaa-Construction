10875200f2c6249124898fb5e46eae7e2c4cb4ef
Saravanan Sathishkumar <163372086+Saravanan2005real@users.noreply.github.com>
2026-07-07 00:19:15 +0530

    feat: remove Facebook icon and profile link from footer

--- frontend/components/layout/SocialIcons.tsx
+++ frontend/components/layout/SocialIcons.tsx
@@ -4,19 +4,6 @@ type IconProps = {
   className?: string;
 };
 
-export function FacebookIcon({ className = 'h-[22px] w-[22px]' }: IconProps) {
-  return (
-    <svg
-      className={className}
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      aria-hidden="true"
-    >
-      <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z" />
-    </svg>
-  );
-}
-
 export function InstagramIcon({ className = 'h-[22px] w-[22px]' }: IconProps) {
   return (
     <svg
@@ -66,11 +53,6 @@ type SocialLink = {
 };
 
 const socialLinks: SocialLink[] = [
-  {
-    href: 'https://facebook.com',
-    label: 'Facebook Profile',
-    icon: <FacebookIcon />,
-  },
   {
     href: 'https://www.instagram.com/omegaa_construction?igsh=dGV2M3hiMWFtdjZk&utm_source=qr',
     label: 'Instagram Profile',
